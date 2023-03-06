3ab191dbf0c726dbc5799b63c531290023a603ea
qwertymanuser <marlenrai14@gmail.com>
2023-03-06 19:15:37 +0600

    second commit

--- calc.py
+++ calc.py
@@ -4,31 +4,43 @@ import sys
 class CustomCalculator(QMainWindow):
     def __init__(self):
         super(CustomCalculator, self).__init__()
-        loadUi('untitled.ui', self)
+        loadUi('calc.ui', self)
         self.add.clicked.connect(self.add_bottun)
         self.sub.clicked.connect(self.sub_bottun)
         self.mult.clicked.connect(self.mult_button)
         self.div.clicked.connect(self.div_button)
-        
+    
     def add_bottun(self):
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
-        self.result.setText(f"Ответ: {num1 + num2}")
-    def sub_bottun(self):
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
-        self.result.setText(f"Ответ: {num1 - num2}")
-    def mult_button(self):
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
-        self.result.setText(f"Ответ: {num1 * num2}")
-    def div_button(self):
-        num1 = int(self.input1.text())
-        num2 = int(self.input2.text())
         try:
-            self.result.setText(f"Ответ: {num1 / num2}")
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.result.setText(f"Ответ: {num1 + num2}")
         except:
-            self.result.setText("на ноль нельзя делить")
+            self.result.setText("Введи цифры")
+    def sub_bottun(self):
+        try:
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.result.setText(f"Ответ: {num1 - num2}")
+        except:
+            self.result.setText("Введи цифры")
+    def mult_button(self):
+        try:
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            self.result.setText(f"Ответ: {num1 * num2}")
+        except:
+            self.result.setText("Введи цифры")
+    def div_button(self):
+        try:
+            num1 = int(self.input1.text())
+            num2 = int(self.input2.text())
+            try:
+                self.result.setText(f"Ответ: {num1 / num2}")
+            except:
+                self.result.setText("на ноль нельзя делить")
+        except:
+            self.result.setText("Введи цифры")
 app = QApplication(sys.argv)
 calc = CustomCalculator()
 calc.show()
--- main.py
+++ main.py
@@ -1,47 +1,25 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.uic import loadUi
+from PyQt5 import QtWidgets
 import sys
-class CustomCalculator(QMainWindow):
-    def __init__(self):
-        super(CustomCalculator, self).__init__()
-        loadUi('calc.ui', self)
-        self.add.clicked.connect(self.add_bottun)
-        self.sub.clicked.connect(self.sub_bottun)
-        self.mult.clicked.connect(self.mult_button)
-        self.div.clicked.connect(self.div_button)
-    
-    def add_bottun(self):
-        try:
-            num1 = int(self.input1.text())
-            num2 = int(self.input2.text())
-            self.result.setText(f"Ответ: {num1 + num2}")
-        except:
-            self.result.setText("Введи цифры")
-    def sub_bottun(self):
-        try:
-            num1 = int(self.input1.text())
-            num2 = int(self.input2.text())
-            self.result.setText(f"Ответ: {num1 - num2}")
-        except:
-            self.result.setText("Введи цифры")
-    def mult_button(self):
-        try:
-            num1 = int(self.input1.text())
-            num2 = int(self.input2.text())
-            self.result.setText(f"Ответ: {num1 * num2}")
-        except:
-            self.result.setText("Введи цифры")
-    def div_button(self):
-        try:
-            num1 = int(self.input1.text())
-            num2 = int(self.input2.text())
-            try:
-                self.result.setText(f"Ответ: {num1 / num2}")
-            except:
-                self.result.setText("на ноль нельзя делить")
-        except:
-            self.result.setText("Введи цифры")
-app = QApplication(sys.argv)
-calc = CustomCalculator()
-calc.show()
-app.exec_()
+
+def push_button():
+    print("hello world")
+
+def application():
+    app = QApplication(sys.argv)
+    window = QMainWindow()
+
+    window.setWindowTitle("Python PyQt5")
+    window.setGeometry(400, 400, 400, 400)
+
+    main_text = QtWidgets.QLabel(window)
+    main_text.setText("Hello World")
+    main_text.move(150, 150)
+
+    main_button = QtWidgets.QPushButton(window)
+    main_button.setText("Click")
+    main_button.move(135,190)
+    main_button.clicked.connect(push_button)
+    window.show()
+    sys.exit(app.exec_())
+application()
